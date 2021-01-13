fe836c3dfca17664dd93f7509e16a058a38692d0
Sebastião Junior <sebastiaojuniordev@gmail.com>
2021-01-13 19:47:15 -0300

    First code.

---
+++ src/test/java/br/com/sebastiao/junior/CrudOperationsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sebastiao.junior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudOperationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
